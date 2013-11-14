f678e21c4e7e16045cd3a292cec31f07c8a0641a
Timothy Miller <timothymiller@seas1520.wireless-pennnet.upenn.edu>
2013-11-14 12:15:55 -0500

    adding class changes

--- src/airplane/classes/LocationList.java
+++ src/airplane/classes/LocationList.java
@@ -1,5 +1,6 @@
 package airplane.classes;
 
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
 
 import org.apache.log4j.Logger;
@@ -16,6 +17,7 @@ public class LocationList {
 	public void insertLoc(PlaneDetails d)			{locs.add(d);}
 	public void setLocAt(int t, PlaneDetails d) 	{locs.add(t, d);}
 	public int size()								{return locs.size();}
-	public PlaneDetails getLocAt(int t) 			{return locs.get(t);}
+	public Point2D.Double getLocAt(int t) 			{return locs.get(t).getLoc();}
+	public double getBearingAt(int t)				{return locs.get(t).getBearing();}
 	
 } 
